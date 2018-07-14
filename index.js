1297f3854e9f0ece4e08ced6520e1ffb7dcfabd8
john.rainbird@gmail.com <john.rainbird@gmail.com>
2018-07-14 07:34:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,11 +22,14 @@ function sayHiToGrandma(string) {
   if (lowercase.toLowerCase() === lowercase) {
     return "I can't hear you!"
   
+  
+  
   } else if (uppercase.toUpperCase() === uppercase) {
     return "YES INDEED!"
     
-  } else if (string === "I love you, Grandma.") {
+  } else if (mixedCase === mixedCase) {
     return "I love you, too."
+    
   }
 
 
